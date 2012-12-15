fa971828fc41a0104dbce300707b19ee6cf66bba
Jason Viers <bean@beanalby.net>
2012-12-14 23:45:47 -0500

    Added proper Muffin rigidbody movement and jumping

--- Unity/Assets/Scripts/MuffinController.cs
+++ Unity/Assets/Scripts/MuffinController.cs
@@ -3,31 +3,75 @@ using System.Collections;
 
 public class MuffinController : MonoBehaviour {
 
-    private Vector3 velocity = Vector3.zero;
-    private float speed = 10;
+    private Vector3 gravity = Physics.gravity * 2;
 
+    private float jumpSpeed = 12;
+    private float moveSpeed = 10;
+    private Vector3 camOffset = new Vector3(6, 8, -12);
+    private float cameraSnap = .2f;
+    private float distToGround = 3f;
 
     private Rigidbody rb;
+    private Camera cam;
 
-	// Use this for initialization
-	void Start () {
+    private bool doJump;
+    private Vector3 velocity = Vector3.zero;
+
+	void Start ()
+    {
         rb = GetComponent<Rigidbody>();
+        cam = Camera.mainCamera;
 	}
-	
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            Debug.Log("Setting doJump");
+            doJump = true;
+        }
+    }
+
 	// Update is called once per frame
-	void Update () {
-        float factor = speed * Time.deltaTime;
-        rb.MovePosition(rb.position + new Vector3(factor * Input.GetAxis("Horizontal"), 0, 0));
+	void FixedUpdate ()
+    {
+        // figure out where our new position will be
+        Vector3 newPos;
+        velocity.x = moveSpeed * Input.GetAxis("Horizontal");
+        if (doJump)
+        {
+            velocity.y = jumpSpeed;
+            doJump = false;
+        }
+        newPos = rb.position + (velocity * Time.deltaTime);
+
+        // increase downward velocity by gravity
+        if (!IsGrounded())
+           velocity += Time.deltaTime * gravity;
+        // don't go down through the ground
+        if (newPos.y - distToGround < 0)
+        {
+            newPos.y = distToGround;
+            velocity.y = 0;
+        }
+
+        rb.MovePosition(newPos);
+
+        // have the camera follow muffin horizontally
+        Vector3 camDestination = camOffset + new Vector3(rb.position.x, 0, 0);
+        cam.transform.position = Vector3.Lerp(cam.transform.position, camDestination, cameraSnap);
 	}
 
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        if (hit.normal == Vector3.up)
-            return;
-        Debug.Log("OnControllerColliderHit with " + hit.gameObject.name);
     }
     void OnCollisionEnter(Collision info)
     {
         Debug.Log("OnCollisionEnter with " + info.gameObject.name);
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(rb.position, -Vector3.up, distToGround + .1f);
     }
 }
